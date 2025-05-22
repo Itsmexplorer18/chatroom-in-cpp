69529182705d8fdf379940db19eb23582570014a
Harshita Ghughutiyal <124561481+Itsmexplorer18@users.noreply.github.com>
2025-05-22 15:05:20 +0530

    Update message.hpp

--- message.hpp
+++ message.hpp
@@ -5,9 +5,9 @@
 #include <cstdio>
 #include <string>
 #include <iostream>
-class Message{
-    public:
-         Message() : bodyLength_(0) {}
+class Message {
+    public: 
+        Message() : bodyLength_(0) {}
         
         enum {maxBytes = 512};
         enum {header = 4};
@@ -18,4 +18,56 @@ class Message{
             std::memcpy(data + header, message.c_str(), bodyLength_);
         };
 
-    };
+        void printMessage(){
+            std::string message = getData();
+            std::cout<<"Message recieved: "<<message<<std::endl;
+        }
+
+        std::string getData(){
+            int length = header + bodyLength_;
+            std::string result(data, length);
+            return result;
+        }
+
+        std::string getBody(){
+            std::string dataStr = getData();
+            std::string result = dataStr.substr(header, bodyLength_);
+            return result;
+        }
+
+        size_t getNewBodyLength(size_t newLength){
+            if(newLength > maxBytes){
+                return maxBytes;
+            }
+            return newLength;
+        }
+
+        void encodeHeader(){
+            char new_header[header+1] = "";
+            sprintf(new_header, "%4d", static_cast<int>(bodyLength_));
+            memcpy(data, new_header, header);
+        }
+        
+       bool decodeHeader(){
+            char new_header[header+1] = "";
+            strncpy(new_header, data, header);
+            new_header[header] = '\0';
+            int headerValue = atoi(new_header);
+            if(headerValue > maxBytes){
+                bodyLength_ = 0;
+                return false;
+            }
+            bodyLength_ = headerValue;
+            return true;
+        }
+
+        size_t getBodyLength(){
+            return bodyLength_;
+        }
+
+    private: 
+        char data[header+maxBytes];
+        size_t bodyLength_;
+};
+
+#endif MESSAGE_HPP
