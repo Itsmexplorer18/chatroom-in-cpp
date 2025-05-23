ef27301d6c31fca752f3c9174fa1f2ce29993d96
Harshita Ghughutiyal <124561481+Itsmexplorer18@users.noreply.github.com>
2025-05-23 16:09:22 +0530

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -1,52 +1,54 @@
 #include "chatroom.hpp"
-
 void Room::join(ParticipantPointer participant){
     this->participants.insert(participant);
 }
-
 void Room::leave(ParticipantPointer participant){
     this->participants.erase(participant);
 }
-
 void Room::deliver(ParticipantPointer participant, Message &message){
     messageQueue.push_back(message);
     while (!messageQueue.empty()) {
         Message msg = messageQueue.front();
         messageQueue.pop_front(); 
         
-        for (ParticipantPointer _participant : participants) {
-            if (participant != _participant) {
-                _participant->write(msg);
+        for (ParticipantPointer curr_participant : participants) {
+            if (participant != curr_participant) {
+                curr_participant->write(msg);
             }
+        }
+    }
+}
+void Session::handle_read(boost::system::error_code ec, std::size_t bytes_transferred) {
+    if (!ec) {
+        std::string data(boost::asio::buffers_begin(buffer.data()), 
+                         boost::asio::buffers_begin(buffer.data()) + bytes_transferred);
+                buffer.consume(bytes_transferred);
+        
+        std::cout << "Received: " << data << std::endl;
+                Message message(data);
+        deliver(message);
+                async_read(); 
+    } else {
+        room.leave(shared_from_this());
+        if (ec == boost::asio::error::eof) {
+            std::cout << "Connection closed by peer" << std::endl;
+        } else {
+            std::cout << "Read error: " << ec.message() << std::endl;
         }
     }
 }
 
 void Session::async_read() {
     auto self(shared_from_this());
-    boost::asio::async_read_until(clientSocket, buffer, "\n",
+        boost::asio::async_read_until(
+        clientSocket,   
+        buffer,         
+        "\n",                         
         [this, self](boost::system::error_code ec, std::size_t bytes_transferred) {
-            if (!ec) {
-                std::string data(boost::asio::buffers_begin(buffer.data()), 
-                                 boost::asio::buffers_begin(buffer.data()) + bytes_transferred);
-                buffer.consume(bytes_transferred);
-                std::cout << "Received: " << data << std::endl;
-                
-                Message message(data);
-                deliver(message);
-                async_read(); 
-            } else {
-                room.leave(shared_from_this());
-                if (ec == boost::asio::error::eof) {
-                    std::cout << "Connection closed by peer" << std::endl;
-                } else {
-                    std::cout << "Read error: " << ec.message() << std::endl;
-                }
-            }
+            handle_read(ec, bytes_transferred);
         }
     );
 }
-
 
 void Session::async_write(std::string messageBody, size_t messageLength){
     auto write_handler = [&](boost::system::error_code ec, std::size_t bytes_transferred){
@@ -116,41 +118,3 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
-//understanding the server functions
-/*void accept_connection(tcp::acceptor &acceptor, Room &room) {
-    acceptor.async_accept([&](boost::system::error_code ec, tcp::socket socket) {
-        if(!ec) {
-            std::shared_ptr<Session> session = std::make_shared<Session>(std::move(socket), room);
-            session->start();
-        }
-        // Recursively accept the next connection
-        accept_connection(acceptor, room);
-    });
-}
-
-int main(int argc, char *argv[]) {
-    try {
-        if(argc < 2) {
-            std::cerr << "Usage: server <port>\n";
-            return 1;
-        }
-        
-        Room room;
-        boost::asio::io_context io_context;
-        
-        // Create acceptor and bind to the specified port
-        int port = std::atoi(argv[1]);
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
-        
-        // Start accepting connections
-        accept_connection(acceptor, room);
-        
-        // Run the event loop
-        io_context.run();
-    }
-    catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
-    return 0;
-}
-*/
