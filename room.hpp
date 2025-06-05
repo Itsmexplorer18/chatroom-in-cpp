844baf4117dbf5402168dd4a3389b5ec264c6e82
Harshita Ghughutiyal <124561481+Itsmexplorer18@users.noreply.github.com>
2025-06-05 21:28:21 +0530

    Update room.hpp

--- room.hpp
+++ room.hpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #ifndef CHATROOM_HPP
 #define CHATROOM_HPP
 
@@ -13,38 +11,45 @@
 using boost::asio::ip::tcp;
 
 class Participant {
-    public: 
-        virtual void deliver(Message& message) = 0;
-        virtual void write(Message &message) = 0;
-        virtual ~Participant() = default;
+public: 
+    virtual void deliver(Message& message) = 0;
+    virtual void write(Message &message) = 0;
+    virtual ~Participant() = default;
 };
 
 typedef std::shared_ptr<Participant> ParticipantPointer;
 
-class Room{
-    public:
-        void join(ParticipantPointer participant);
-        void leave(ParticipantPointer participant);
-        void deliver(ParticipantPointer participantPointer, Message &message);
-    private:
-        std::deque<Message> messageQueue;
-        enum {maxParticipants = 100};
-        std::set<ParticipantPointer> participants;
+class Room {
+public:
+    void join(ParticipantPointer participant);
+    void leave(ParticipantPointer participant);
+    void deliver(ParticipantPointer participantPointer, Message &message);
+
+private:
+    std::deque<Message> messageQueue;
+    enum {maxParticipants = 100};
+    std::set<ParticipantPointer> participants;
 };
 
-class Session: public Participant, public std::enable_shared_from_this<Session>{
-    public:
-        Session(tcp::socket s, Room &room);
-        void start();
-        void deliver(Message& message);
-        void write(Message &message);
-        void async_read();
-        void async_write(std::string messageBody, size_t messageLength);
-    private:
-        tcp::socket clientSocket;
-        boost::asio::streambuf buffer;
-        Room& room;
-        std::deque<Message> messageQueue; 
+class Session : public Participant, public std::enable_shared_from_this<Session> {
+public:
+    Session(tcp::socket s, Room &room);
+    void start();
+    void deliver(Message& message) override;
+    void write(Message &message) override;
+    
+    // Provide access to socket for acceptor
+    tcp::socket& socket() { return clientSocket; }
+
+private:
+    void read_header();
+    void read_body();
+    void do_write_();
+    
+    tcp::socket clientSocket;
+    Room& room;
+    Message readMessage_;
+    std::deque<Message> writeQueue_;
 };
 
-#endif CHATROOM_HPP
+#endif // CHATROOM_HPP
